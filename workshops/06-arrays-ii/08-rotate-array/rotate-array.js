bcfc3e0c144bbdb5195546b1f4332cd05a57410a
Hemlal Dulal <dulalhemlal986@gmail.com>
2023-06-12 22:43:03 +0545

    upto array-ii completed

--- workshops/06-arrays-ii/08-rotate-array/rotate-array.js
+++ workshops/06-arrays-ii/08-rotate-array/rotate-array.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+const rotateArray = (originalArray, rotateNum) => {
+  const direction = rotateNum >= 0 ? "right" : "left";
+  const rotatedArray = originalArray.slice();
+  for (let i = 0; i < Math.abs(rotateNum); i++) {
+    if (direction === "right") {
+      rotatedArray.unshift(rotatedArray.pop());
+    } else {
+      rotatedArray.push(rotatedArray.shift());
+    }
+  }
+  return rotatedArray;
+};
+console.log(rotateArray([1, 2, 3, 4, 5], 1));
