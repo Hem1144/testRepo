5bebb9cc0e34ee01f52adf69d200f95da3be9bd4
Hemlal Dulal <dulalhemlal986@gmail.com>
2023-06-16 18:03:18 +0545

    did a miss problems of tidy-codes

--- workshops/01-tidy-code/03-get-max/get-max.js
+++ workshops/01-tidy-code/03-get-max/get-max.js
@@ -1,2 +1,13 @@
 // YOUR CODE BELOW
-function getMax(f, s, a) {if (f > s) {if (f > a) {return f;} return a;} else if (s > a) {return s;}else {return a;}}
+function getMax(number1, number2, number3) {
+  if (number1 > number2) {
+    if (number1 > number3) {
+      return number1;
+    }
+    return number3;
+  } else if (number2 > number3) {
+    return number2;
+  } else {
+    return number3;
+  }
+}
